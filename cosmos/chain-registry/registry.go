5b7b1d5cacd059ceaf330501e344cec36b858701
Keefer Taylor | Tessellated <keefer@tessellated.io>
2023-10-30 19:11:01 +0100

    enable caching

--- cosmos/chain-registry/registry.go
+++ cosmos/chain-registry/registry.go
@@ -10,24 +10,32 @@ import (
 	"time"
 
 	retry "github.com/avast/retry-go/v4"
+	"github.com/tessellated-io/pickaxe/log"
 )
 
 type RegistryClient struct {
 	attempts retry.Option
 	delay    retry.Option
 
-	// TODO
-	// Cache of all chains
-	// allChainNames []string
+	// Cache of all chain names
+	chainNames []string
+
+	// Cache of chain names to chain ID
+	chainNameToChainID map[string]string
+
+	log log.Logger
 }
 
-func NewRegistryClient() *RegistryClient {
+func NewRegistryClient(log log.Logger) *RegistryClient {
 	return &RegistryClient{
 		attempts: retry.Attempts(5),
 		delay:    retry.Delay(1 * time.Second),
 
 		// Initially empty chain name cache
-		// allChainNames: []string{},
+		chainNames:         []string{},
+		chainNameToChainID: make(map[string]string),
+
+		log: log,
 	}
 }
 
@@ -64,28 +72,46 @@ func (rc *RegistryClient) ChainNameForChainID(ctx context.Context, chainID strin
 
 // TODO: use cache param
 // TODO: enable caching in this method
-func (rc *RegistryClient) chainNameForChainID(ctx context.Context, chainID string, refreshCache bool) (string, error) {
-	// Get all chain names
-	url := "https://cosmos-chain.directory/chains"
-	bytes, err := rc.makeRequest(ctx, url)
-	if err != nil {
-		return "", err
+func (rc *RegistryClient) chainNameForChainID(ctx context.Context, targetChainID string, refreshCache bool) (string, error) {
+	// If refresh cache is requested, clear the local values
+	if refreshCache {
+		rc.chainNames = []string{}
+		rc.chainNameToChainID = make(map[string]string)
+		rc.log.Debug().Msg("reset chain names and chain ids caches")
 	}
 
-	chainNames, err := parseAllChainsResponse(bytes)
-	if err != nil {
-		return "", err
-	}
+	// Fetch chain names if they are not cached, or if we requested a refetch from the cache
+	chainNames := rc.chainNames
+	if len(chainNames) == 0 {
+		rc.log.Debug().Msg("no cached name found, reloading from registry")
 
-	// For each chain name, cache the chain id
-	for _, chainName := range chainNames {
-		// Fetch the chain ID for the chain
-		chainInfo, err := rc.GetChainInfo(ctx, chainName)
+		var err error
+		chainNames, err = rc.getAllChainNames(ctx)
 		if err != nil {
 			return "", err
 		}
 
-		if strings.EqualFold(chainInfo.ChainID, chainID) {
+		rc.log.Debug().Int("num_chains", len(chainNames)).Msg("loaded chains from the registry")
+		rc.chainNames = chainNames
+	}
+
+	// For each chain name, get the chain id from the cache or from fetching
+	for _, chainName := range chainNames {
+		rc.log.Debug().Str("chain_name", chainName).Msg("processing chain")
+
+		chainID, isSet := rc.chainNameToChainID[chainName]
+		if !isSet {
+			// Fetch the chain ID for the chain
+			// NOTE: No retries because GetChainInfo manages that for us.
+			chainInfo, err := rc.GetChainInfo(ctx, chainName)
+			if err != nil {
+				return "", err
+			}
+
+			chainID = chainInfo.ChainID
+		}
+
+		if strings.EqualFold(chainName, chainID) {
 			return chainName, nil
 		}
 	}
@@ -106,6 +132,23 @@ func (rc *RegistryClient) getChainInfo(ctx context.Context, chainName string) (*
 		return nil, err
 	}
 	return chainInfo, nil
+}
+
+// Internal method without retries
+func (rc *RegistryClient) getAllChainNames(ctx context.Context) ([]string, error) {
+	// Get all chain names
+	url := "https://cosmos-chain.directory/chains"
+	bytes, err := rc.makeRequest(ctx, url)
+	if err != nil {
+		return nil, err
+	}
+
+	chainNames, err := parseAllChainsResponse(bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return chainNames, nil
 }
 
 func (rc *RegistryClient) makeRequest(ctx context.Context, url string) ([]byte, error) {
