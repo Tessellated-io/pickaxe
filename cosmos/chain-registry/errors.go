73b2772a4358b13385793bb5c0d8e39ba7cf6100
Keefer Taylor | Tessellated <keefer@tessellated.io>
2023-12-08 15:32:39 -0800

    chain config

--- cosmos/chain-registry/errors.go
+++ cosmos/chain-registry/errors.go
@@ -3,3 +3,5 @@ package registry
 import "errors"
 
 var ErrNoChainFoundForChainID = errors.New("no chain found for chain ID")
+var ErrNoStakingTokenFound = errors.New("no staking tokens found in registry")
+var ErrNoFeeTokenFound = errors.New("no fee tokens found in registry")
