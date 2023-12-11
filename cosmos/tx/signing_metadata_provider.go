1874909d9125a45e9522b9e2fc1c82c66c9b4695
Keefer Taylor | Tessellated <keefer@tessellated.io>
2023-12-11 13:17:09 -0800

    lint

--- cosmos/tx/signing_metadata_provider.go
+++ cosmos/tx/signing_metadata_provider.go
@@ -16,7 +16,7 @@ func NewSigningMetadataProvider(rpcClient rpc.RpcClient) (*SigningMetadataProvid
 	}, nil
 }
 
-func (sip *SigningMetadataProvider) SigningMetadataForAccount(ctx context.Context, address string, chainID string) (*SigningMetadata, error) {
+func (sip *SigningMetadataProvider) SigningMetadataForAccount(ctx context.Context, address, chainID string) (*SigningMetadata, error) {
 	account, err := sip.rpcClient.Account(ctx, address)
 	if err != nil {
 		return nil, err
