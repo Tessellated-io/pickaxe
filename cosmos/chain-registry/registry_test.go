bb10e6973fba9f357b634df841d3d9c08c197efa
Keefer Taylor | Tessellated <keefer@tessellated.io>
2024-01-23 01:35:04 -0800

    rework chain registry

--- cosmos/chain-registry/registry_test.go
+++ cosmos/chain-registry/registry_test.go
@@ -10,11 +10,25 @@ import (
 	"github.com/tessellated-io/pickaxe/log"
 )
 
-func TestChainRegistryIsAlive(t *testing.T) {
-	client := registry.NewChainRegistryClient(log.NewLogger(zerolog.FatalLevel))
+func TestCanRetrieveChain(t *testing.T) {
+	client := registry.NewChainRegistryClient(log.NewLogger(zerolog.FatalLevel), "https://chain-registry.tessellated.io")
 
 	hubInfo, err := client.ChainInfo(context.Background(), "cosmoshub")
 	assert.Nil(t, err, "error should be nil")
 
 	assert.Equal(t, hubInfo.ChainID, "cosmoshub-4", "incorrect chain id")
+}
+
+func TestCanRetrieveAssets(t *testing.T) {
+	client := registry.NewChainRegistryClient(log.NewLogger(zerolog.FatalLevel), "https://chain-registry.tessellated.io")
+
+	_, err := client.AssetList(context.Background(), "cosmoshub")
+	assert.Nil(t, err, "error should be nil")
+}
+
+func TestCanRetrieveAllChains(t *testing.T) {
+	client := registry.NewChainRegistryClient(log.NewLogger(zerolog.FatalLevel), "https://chain-registry.tessellated.io")
+
+	_, err := client.AllChainNames(context.Background())
+	assert.Nil(t, err, "error should be nil")
 }
